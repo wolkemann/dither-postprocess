cf721e5c4b678503935ccf252dbf9a7472c75ff5
Federico Sardo <ohtoristudent@gmail.com>
2026-01-19 00:23:45 +0100

    many changes ;D

--- src/main.js
+++ src/main.js
@@ -11,6 +11,8 @@ import { Pane } from "tweakpane";
 import { fillPallette, rgbToHTMLColor } from "./utils";
 
 import "./style.css";
+import { DEV_MODE, LIGHT_INTENSITY } from "./constants";
+import gsap from "gsap";
 
 const loader = new GLTFLoader();
 
@@ -18,31 +20,122 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   50,
   window.innerWidth / window.innerHeight,
-  0.1,
-  1000,
+  0.01,
+  2000,
 );
 
 const renderer = new THREE.WebGLRenderer({ antialias: false });
-renderer.outputColorSpace = THREE.LinearSRGBColorSpace;
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
 const controls = new OrbitControls(camera, renderer.domElement);
+controls.mouseButtons = {
+  LEFT: THREE.MOUSE.ROTATE,
+  MIDDLE: THREE.MOUSE.DOLLY,
+  RIGHT: DEV_MODE ? THREE.MOUSE.PAN : undefined,
+};
+controls.enableZoom = false;
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+/* ***********************************************************************************
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshToonMaterial({ color: 0x00ffff });
-const cube = new THREE.Mesh(geometry, material);
+  Lights
 
-cube.rotation.y = 2;
+*********************************************************************************** */
+function createSpotLight(
+  intensity,
+  position,
+  targetPosition,
+  angle,
+  helperColor,
+) {
+  const spotLight = new THREE.SpotLight(0xffffff, intensity, 0);
+  spotLight.angle = angle;
+  spotLight.position.set(position.x, position.y, position.z);
+  spotLight.target.position.set(
+    targetPosition.x,
+    targetPosition.y,
+    targetPosition.z,
+  );
+  scene.add(spotLight);
+  const spotlightHelper = new THREE.SpotLightHelper(spotLight, helperColor);
+  if (DEV_MODE) scene.add(spotlightHelper);
 
+  return { spotLight, spotlightHelper };
+}
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 2.0);
+scene.add(ambientLight);
+
+createSpotLight(
+  LIGHT_INTENSITY,
+  new THREE.Vector3(-0.5, 2.7, 4.24),
+  new THREE.Vector3(5, 0.8, 4.28),
+  Math.PI / 6.7,
+  "red",
+);
+
+createSpotLight(
+  LIGHT_INTENSITY,
+  new THREE.Vector3(2.8, 1.7, 3.24),
+  new THREE.Vector3(3.2, -0.9, -2.2),
+  Math.PI / 8,
+  "blue",
+);
+
+createSpotLight(
+  LIGHT_INTENSITY,
+  new THREE.Vector3(1.7, 1.7, 5.26),
+  new THREE.Vector3(-0.9, -23.1, 60),
+  Math.PI / 8,
+  "orange",
+);
+
+createSpotLight(
+  LIGHT_INTENSITY,
+  new THREE.Vector3(-2.9, 1.7, 5.26),
+  new THREE.Vector3(1.2, -17, 60),
+  Math.PI / 5,
+  "cyan",
+);
+
+createSpotLight(
+  LIGHT_INTENSITY,
+  new THREE.Vector3(0.2, 1.7, 3.2),
+  new THREE.Vector3(0.3, 0.2, 0.0),
+  Math.PI / 8,
+  "green",
+);
+
+createSpotLight(
+  LIGHT_INTENSITY,
+  new THREE.Vector3(-1.9, 1.7, 3.2),
+  new THREE.Vector3(-2.2, 0.2, 0.0),
+  Math.PI / 8,
+  "indigo",
+);
+
+createSpotLight(
+  LIGHT_INTENSITY,
+  new THREE.Vector3(-2.0, 2.8, 4.3),
+  new THREE.Vector3(-34.0, -50.8, 4.9),
+  Math.PI / 8,
+  "violet",
+);
+/* ***********************************************************************************
+
+  Model Loading
+
+*********************************************************************************** */
 loader.load(
-  "car/car.glb",
+  "art_museum_vr.glb",
   (gltf) => {
     const model = gltf.scene;
-
-    model.scale.set(140, 140, 140);
-    model.position.set(0, -0.5, 0);
-    model.rotation.set(0.2, -0.3, 0);
     scene.add(model);
+
+    camera.position.set(-0.31, -0.932, 4.355);
+    controls.target.set(0.345, -0.897, 4.36);
+    controls.update();
   },
   undefined,
   (error) => {
@@ -50,44 +143,260 @@ loader.load(
   },
 );
 
+loader.load(
+  "julius_casar.glb",
+  (gltf) => {
+    const model = gltf.scene;
+    model.position.set(-4, -1.63, 4.7);
+    model.rotation.set(0, Math.PI / 2, 0);
+
+    scene.add(model);
+  },
+  undefined,
+  (error) => {
+    console.error(error);
+  },
+);
+/* ***********************************************************************************
+
+ Apply Post Processing
+
+*********************************************************************************** */
 const composer = new EffectComposer(renderer);
 const renderPass = new RenderPass(scene, camera);
 composer.addPass(renderPass);
 
 const ditherPass = new ShaderPass(DitherShader);
-camera.position.z = 6;
-
-const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(4.7, 7.7, 6.6);
-pointLight.intensity = 72;
-pointLight.decay = 0.3;
-pointLight.distance = 20;
-scene.add(pointLight);
-const sphereSize = 1;
-const pointLightHelper = new THREE.PointLightHelper(pointLight, sphereSize);
-scene.add(pointLightHelper);
-
-renderer.setAnimationLoop(animate);
 
 let clock = new THREE.Clock();
 let delta = 0;
 let interval = 1 / 30;
+
+renderer.setAnimationLoop(animate);
 
 function animate() {
   requestAnimationFrame(animate);
   delta += clock.getDelta();
 
   if (delta > interval) {
+    controls.update();
     composer.render();
 
     delta = delta % interval;
   }
 }
+/* ***********************************************************************************
 
+  Area trigger event logic
+
+*********************************************************************************** */
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
+const artworkTriggers = [];
+
+function moveToArtwork(cameraPos, targetPos) {
+  gsap.to(camera.position, {
+    x: cameraPos.x,
+    y: cameraPos.y,
+    z: cameraPos.z,
+    duration: 1.5,
+    ease: "power3.inOut",
+    onUpdate: () => controls.update(),
+    onComplete: () => camera.updateProjectionMatrix(),
+  });
+
+  gsap.to(controls.target, {
+    x: targetPos.x,
+    y: targetPos.y,
+    z: targetPos.z,
+    duration: 1.5,
+    ease: "power3.inOut",
+  });
+}
+
+function addArtworkTrigger(
+  x,
+  y,
+  z,
+  width,
+  height,
+  rotationY,
+  areaName,
+  depth = 0.1,
+) {
+  const geo = new THREE.BoxGeometry(width, height, depth);
+  const mat = new THREE.MeshBasicMaterial({
+    color: 0x00ff00,
+    transparent: true,
+    opacity: DEV_MODE ? 0.5 : 0.0,
+    side: THREE.DoubleSide,
+  });
+
+  const trigger = new THREE.Mesh(geo, mat);
+  trigger.position.set(x, y, z);
+  trigger.rotation.y = rotationY;
+  trigger.userData.areaName = areaName;
+
+  scene.add(trigger);
+  artworkTriggers.push(trigger);
+
+  return trigger;
+}
+
+const areaTrigger = addArtworkTrigger(3, -0.1, 0.1, 2.4, 1.4, 0, "Taco");
+const portrait2Trigger = addArtworkTrigger(
+  0.3,
+  0,
+  0.1,
+  2.4,
+  1.35,
+  0,
+  "portrait 2",
+);
+const portrait3Trigger = addArtworkTrigger(
+  -2.2,
+  0,
+  0.1,
+  2.4,
+  1.35,
+  0,
+  "portrait 3",
+);
+const portrait4Trigger = addArtworkTrigger(
+  4.9,
+  0,
+  4.3,
+  5,
+  3.0,
+  Math.PI / -2,
+  "big front picture",
+);
+const portrait5Trigger = addArtworkTrigger(
+  1.5,
+  0,
+  8.4,
+  1.5,
+  3.0,
+  Math.PI,
+  "vertical portrait",
+);
+const portrait6Trigger = addArtworkTrigger(
+  -2.45,
+  0,
+  8.4,
+  4.5,
+  2.4,
+  Math.PI,
+  "second big portrait",
+);
+const statueTrigger = addArtworkTrigger(
+  -4,
+  -0.8,
+  4.3,
+  1,
+  1.6,
+  Math.PI / 2,
+  "statue",
+  1.2,
+);
+
+/* ***********************************************************************************
+
+  Event Listeners
+
+*********************************************************************************** */
 window.addEventListener("resize", () => {
   camera.aspect = window.innerWidth / window.innerHeight;
   renderer.setSize(window.innerWidth, window.innerHeight);
   camera.updateProjectionMatrix();
+});
+
+window.addEventListener("mousemove", (event) => {
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+});
+
+// zoom on the point x2 when z is pressed
+window.addEventListener("keydown", (e) => {
+  if (e.key === "z" || e.key === "Z") {
+    gsap.to(camera, {
+      fov: 30,
+      duration: 0.5,
+      ease: "power3.inOut",
+      onUpdate: () => {
+        controls.update();
+        camera.updateProjectionMatrix();
+      },
+    });
+  }
+});
+
+window.addEventListener("keyup", (e) => {
+  if (e.key === "p" || e.key === "P") {
+    console.log("--- CAMERA SETTINGS ---");
+    console.log("Position:", camera.position);
+    console.log("Target (Controls):", controls.target);
+
+    navigator.clipboard.writeText(
+      `camera.position.set(${camera.position.x}, ${camera.position.y}, ${camera.position.z}); controls.target.set(${controls.target.x}, ${controls.target.y}, ${controls.target.z});`,
+    );
+  }
+
+  if (e.key === "e" || e.key === "E") {
+    raycaster.setFromCamera(new THREE.Vector2(0, 0), camera);
+    const intersects = raycaster.intersectObjects(artworkTriggers);
+
+    if (intersects.length > 0) {
+      const quad = intersects[0].object;
+      console.log(intersects[0].object.userData.areaName);
+
+      let offset = new THREE.Vector3(0, 0, 1.5);
+
+      if (quad.userData.areaName === "big front picture")
+        offset = new THREE.Vector3(0, 0, 2.9);
+
+      if (quad.userData.areaName === "second big portrait")
+        offset = new THREE.Vector3(0, 0, 2.5);
+
+      offset.applyQuaternion(quad.quaternion);
+
+      const newCameraPos = new THREE.Vector3().copy(quad.position).add(offset);
+
+      moveToArtwork(newCameraPos, quad.position);
+    }
+  }
+
+  if (e.key === "r" || e.key === "R") {
+    // Reset camera position
+    gsap.to(camera.position, {
+      x: -0.31,
+      y: -0.932,
+      z: 4.355,
+      duration: 1.5,
+      ease: "power3.inOut",
+      onUpdate: () => controls.update(),
+    });
+
+    gsap.to(controls.target, {
+      x: 0.345,
+      y: -0.897,
+      z: 4.36,
+      duration: 1.5,
+      ease: "power3.inOut",
+    });
+  }
+
+  if (e.key === "z" || e.key === "Z") {
+    gsap.to(camera, {
+      fov: 50,
+      duration: 0.5,
+      ease: "power3.inOut",
+      onUpdate: () => {
+        controls.update();
+        camera.updateProjectionMatrix();
+      },
+    });
+  }
 });
 
 /* ***********************************************************************************
@@ -105,12 +414,6 @@ DEFAULT_PALETTE.forEach((color, index) => {
 });
 
 let config = {
-  pointLight: {
-    position: { x: 4.7, y: 7.7, z: 6.6 },
-    distance: 20,
-    intensity: 72,
-    decay: 0.3,
-  },
   postProcessSettings: {
     width: 8,
     height: 8,
@@ -122,44 +425,6 @@ let config = {
 };
 
 const pane = new Pane();
-
-const lightFolder = pane.addFolder({ title: "PointLight", expanded: true });
-lightFolder
-  .addBinding(config.pointLight, "position", {
-    min: 0.0,
-    max: 99,
-    step: 0.1,
-  })
-  .on("change", (ev) => {
-    pointLight.position.set(ev.value.x, ev.value.y, ev.value.z);
-  });
-lightFolder
-  .addBinding(config.pointLight, "distance", {
-    min: -20,
-    max: 20,
-    step: 0.1,
-  })
-  .on("change", (ev) => {
-    pointLight.distance = ev.value;
-  });
-lightFolder
-  .addBinding(config.pointLight, "intensity", {
-    min: 0,
-    max: 200,
-    step: 1,
-  })
-  .on("change", (ev) => {
-    pointLight.intensity = ev.value;
-  });
-lightFolder
-  .addBinding(config.pointLight, "decay", {
-    min: 0,
-    max: 5,
-    step: 0.1,
-  })
-  .on("change", (ev) => {
-    pointLight.decay = ev.value;
-  });
 
 const shaderToggler = pane.addFolder({
   title: "Post Process Toggle",
